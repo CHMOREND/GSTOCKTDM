c5faddc5176471716195730ea3c23d119b73494b
CHMOREND <55686948+CHMOREND@users.noreply.github.com>
2020-03-22 11:01:49 +0100

    Modification MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         this.btinventaire = (Button) findViewById(R.id.BtInventaire);
         this.btentreemarchandise = (Button) findViewById(R.id.BtEntreeMarchandise);
         this.btsortiemarchandise = (Button) findViewById(R.id.BtSoriteMarchandise);
+
+        //SQLiteDatabase db = new SQL
 
 
 
